7d07e1d1840eaabdbe602de1d80af8d3ff11a940
vijaygalent <vijay.anbu@galent.com>
2026-01-06 14:34:05 +0530

    Add vet_id column to pets table in MySQL schema

--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -41,9 +41,11 @@ CREATE TABLE IF NOT EXISTS pets (
   birth_date DATE,
   type_id INT(4) UNSIGNED NOT NULL,
   owner_id INT(4) UNSIGNED,
+  vet_id INT(4) UNSIGNED,
   INDEX(name),
   FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
+  FOREIGN KEY (type_id) REFERENCES types(id),
+  FOREIGN KEY (vet_id) REFERENCES vets(id)
 ) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS visits (
